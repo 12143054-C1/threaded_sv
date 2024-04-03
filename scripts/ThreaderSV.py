3ee3f89d2504c4d0e23cd77dbbf372d2866e13ab
12143054-C1 <sivan.zusin@intel.com>
2024-04-03 03:45:43 +0300

    workin with Queues

--- scripts/ThreaderSV.py
+++ scripts/ThreaderSV.py
@@ -1,16 +1,14 @@
-'''
-Main module. calls all the other modules, creates threads.
-'''
-import threading,sys,os
+from queue import Queue
+import threading
 import tkinter as tk
 from tkinter import simpledialog, ttk
-from queue import Queue
 
-global_setup_tasks = []
-global_gui_tasks = []
+setup_queue = Queue()
+task_queue  = Queue()
+
 
 class SimpleGUI:
-    def __init__(self, root, empty_task_list):
+    def __init__(self, root, setup_queue,empty_task_list):
         self.root = root
         self.root.title("Scrollable Table GUI")
 
@@ -30,12 +28,6 @@ class SimpleGUI:
         self.scrollbar = ttk.Scrollbar(root, orient=tk.VERTICAL, command=self.tree.yview)
         self.scrollbar.pack(side=tk.LEFT, fill='y')  # Pack the scrollbar next to the treeview
         self.tree.configure(yscrollcommand=self.scrollbar.set)
-
-        # Insert the items into the treeview
-        if not empty_task_list:
-            global global_setup_tasks
-            for item in global_setup_tasks:
-                self.tree.insert('', tk.END, values=item)
 
         # Frame for controls (add/remove buttons)
         self.controls_frame = tk.Frame(root)
@@ -57,6 +49,13 @@ class SimpleGUI:
         self.generate_list_button = tk.Button(self.controls_frame, text="Generate List from Setup", command=self.generate_list)
         self.generate_list_button.grid(row=1, column=1)
 
+        # Queue for tasks
+        self.setup_queue = setup_queue
+        self.task_queue = Queue()
+
+        if not empty_task_list:
+            self.generate_list()
+
     def add_item(self):
         """Function to add a new item to the treeview."""
         item = simpledialog.askstring("Input", "Enter comma-separated values for new item:")
@@ -64,8 +63,7 @@ class SimpleGUI:
             values = item.split(',')
             if len(values) == len(self.columns):  # Check if the entered values match the number of columns
                 self.tree.insert('', tk.END, values=values)
-                global global_gui_tasks
-                global_gui_tasks.append(list(values))
+                self.task_queue.put(values)
             else:
                 tk.messagebox.showerror("Error", f"The number of values entered does not match the number of columns: {len(self.columns)}.")
 
@@ -82,68 +80,72 @@ class SimpleGUI:
 
     def generate_list(self):
         """Function to generate a list from setup."""
-        global global_setup_tasks
-        for values in global_setup_tasks:  # Proceed only if something was entered
+        tmp = Queue()
+        while not self.setup_queue.empty():
+            values = self.setup_queue.get()
+            tmp.put(values)
             if len(values) == len(self.columns):  # Check if the entered values match the number of columns
                 self.tree.insert('', tk.END, values=values)
+        while not tmp.empty():
+            self.setup_queue.put(tmp.get())
 
 class MainClass():
-    def __init__(self,empty_task_list = False) -> None:
+    def __init__(self, empty_task_list=False) -> None:
         self.empty_task_list = empty_task_list
+        self.task_queue = Queue()
         self.DoTechnicals()
         self.configurations = {
-                "global" : {
-                    "scope_ip"        : "0.0.0.0",
-                    "jbert_ip"        : "0.0.0.0",
-                    "switch_ip"       : "0.0.0.0",
-                    "switch_map_path" : r"switch\map\path",
-                    
+            "global": {
+                "scope_ip": "0.0.0.0",
+                "jbert_ip": "0.0.0.0",
+                "switch_ip": "0.0.0.0",
+                "switch_map_path": r"switch\map\path",
+
+            },
+            "tcss": {
+                "global": {
+                    "bin_file_destination_folder": r"bin\dest",
+                    "sigtest_results_folder": r"sigtest\results\dest"
                 },
-                "tcss" : {
-                    "global" : {
-                        "bin_file_destination_folder" : r"bin\dest",
-                        "sigtest_results_folder"      : r"sigtest\results\dest"
-                    },
-                    "TBT10" : {
-                        "scope_preset_path" : r"scope\path",
-                        "jbert_preset_path" : r"jbert\path",
-                        "preset" : 2
-                    },
-                    "TBT20" : {
-                        "scope_preset_path" : r"scope\path",
-                        "jbert_preset_path" : r"jbert\path",
-                        "preset" : 2
-                    },
-                    "DP20" : {
-                        "scope_preset_path" : r"scope\path",
-                        "jbert_preset_path" : r"jbert\path",
-                        "preset" : 1
-                    }
+                "TBT10": {
+                    "scope_preset_path": r"scope\path",
+                    "jbert_preset_path": r"jbert\path",
+                    "preset": 2
                 },
-                "edp" : {
-                    "global"  : {
-                        "jitter_data_folder" : r"jitter\data\folder",
-                        "eye_data_folder"    : r"eye\data\folder",
-                    },
-                    "8.1" : {
-                        "scope_preset_path" : r"scope\path",
-                        "jbert_preset_path" : r"jbert\path",
-                        "preset" : 0
-                    }
+                "TBT20": {
+                    "scope_preset_path": r"scope\path",
+                    "jbert_preset_path": r"jbert\path",
+                    "preset": 2
+                },
+                "DP20": {
+                    "scope_preset_path": r"scope\path",
+                    "jbert_preset_path": r"jbert\path",
+                    "preset": 1
+                }
+            },
+            "edp": {
+                "global": {
+                    "jitter_data_folder": r"jitter\data\folder",
+                    "eye_data_folder": r"eye\data\folder",
+                },
+                "8.1": {
+                    "scope_preset_path": r"scope\path",
+                    "jbert_preset_path": r"jbert\path",
+                    "preset": 0
                 }
             }
-    
+        }
+
     def DoTechnicals(self):
-        global global_setup_tasks
-        global_setup_tasks = self.TaskGenerator()
+        self.setup_tasks = Queue()
+        self.TaskGenerator()
         self.RunGUI()
 
-    def TaskGenerator(self) -> list:
-        tasks = []
-        corners =  ["NOM","LVHT","LVLT","HVLT","HVHT"] 
-        phy_s = ["tcss","edp"]
+    def TaskGenerator(self):
+        corners = ["NOM", "LVHT", "LVLT", "HVLT", "HVHT"]
+        phy_s = ["tcss", "edp"]
         tests = {
-            "tcss"  : [
+            "tcss": [
                 "ui_ssc_eye",
                 "rise_fall_time",
                 "jitter",
@@ -151,19 +153,19 @@ class MainClass():
                 "transmitter_equalization",
                 "electrical_idle_voltage"
             ],
-            "edp"   : ["EHEW","Jitters"],
+            "edp": ["EHEW", "Jitters"],
         }
         instances = {
-            "tcss"  : [1],
-            "edp"   : [0],
+            "tcss": [1],
+            "edp": [0],
         }
         lanes = {
-            "tcss"  : [0,3],
-            "edp"   : [0,2],
+            "tcss": [0, 3],
+            "edp": [0, 2],
         }
         protocols = {
-            "tcss"  : ["TBT20","TBT10","DP20"],
-            "edp"   : ["8.1"],
+            "tcss": ["TBT20", "TBT10", "DP20"],
+            "edp": ["8.1"],
         }
         for corner in corners:
             for phy in phy_s:
@@ -171,20 +173,17 @@ class MainClass():
                     for lane in lanes[phy]:
                         for protocol in protocols[phy]:
                             for test in tests[phy]:
-                                new_task = [corner,phy,instance,lane,protocol,test]
-                                #print(f"Adding task to list: {new_task}")
-                                tasks.append(new_task)
-        return tasks
-    
+                                new_task = [corner, phy, instance, lane, protocol, test]
+                                self.setup_tasks.put(new_task)
+
     def RunGUI(self):
         # Create a new thread targeting the function that initializes and runs the GUI.
         threading.Thread(target=self.init_gui).start()
-    
+
     def init_gui(self):
         root = tk.Tk()
-        app = SimpleGUI(root,self.empty_task_list)
+        app = SimpleGUI(root, self.setup_tasks,self.empty_task_list)
         root.mainloop()
-
 
 if __name__ == "__main__":
     A = MainClass(False)
